a6329119c740e0af7e03b88b84c6abba9b03c9e7
Ishan Joshi <ishan.a.devel@gmail.com>
2020-07-06 09:54:34 +1000

    update: new approach drafted

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -18,6 +18,10 @@ class Solution:
         memo = [0] * nl
         memo[0] = nums[0]
         memo[1] = max(nums[0:2])
+        
+        # This only needs previous two values. The whole memo array is not needed. 
+        # Optimize by storing as a, b where a = memo[i-2], b = memo[i-1]
+        # Res can just be max at all times. :) 
 
         for i in range(2, nl):
 
